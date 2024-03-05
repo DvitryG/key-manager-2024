54e2a8ab854fe8f58b8761d2fda8b1173af9eebe
dmitry <@>
2024-03-05 15:32:43 +0300

    [back] refactor: правка датаклассов заявки

--- backend/routers/order.py
+++ backend/routers/order.py
@@ -1,8 +1,13 @@
 # TODO: CRU for orders
+from datetime import time
 from fastapi import APIRouter
-from pydantic import BaseModel
-from datetime import date, datetime, time, timedelta
-from dto.OrderDto import SimpleOrder, CyclicOrder
+from uuid import UUID
+from backend.models.order import (
+    SimpleOrder,
+    CyclicOrder,
+    OrderStatus,
+    Interval
+)
 
 
 router = APIRouter(
@@ -11,14 +16,14 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-#список заявок пользователя
+
 @router.get("/")
-async def get_my_orders():
+async def get_my_orders() -> list[SimpleOrder | CyclicOrder]:
     pass
 
-#список всех заявок для деканата
+
 @router.get("/all")
-async def get_all_orders():
+async def get_all_orders() -> list[SimpleOrder | CyclicOrder]:
     pass
 
 
@@ -33,5 +38,10 @@ async def create_cyclic_order(order: CyclicOrder):
 
 
 @router.put("/{order_id}/state")
-async def change_order_state():
+async def change_order_state(order_id: UUID, state: OrderStatus):
+    pass
+
+
+@router.get("/intervals")
+async def get_intervals() -> dict[Interval, tuple[time, time]]:
     pass
