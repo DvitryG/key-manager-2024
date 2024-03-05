0a0f15e8d3331b187439af89cf264029ca41dad7
Andrey Alibekov <alibekovand@gmail.com>
2024-03-06 01:01:47 +0700

    [back] создание базы данных

--- backend/debug.py
+++ backend/debug.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,5 @@
-from sqlmodel import SQLModel
+from typing import Optional
+from sqlmodel import Field, SQLModel
 from uuid import UUID, uuid4
 from enum import Enum
 
@@ -10,11 +11,17 @@ class Role(str, Enum):
     STUDENT = "student"
 
 
-class User(SQLModel):
-    user_id: UUID = uuid4()
+class User(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str
-    roles: set[Role]
+    #roles: set[Role]
+
+# class User(BaseModel):
+#     user_id: UUID = uuid4()
+#     name: str
+#     email: str
+#     roles: set[Role]
 
 
 class UserInDB(User):
