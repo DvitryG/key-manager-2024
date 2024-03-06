092a1a2bb054aed122b5f39cee731ccd1e959b96
Sunchugashev Aymir <a_suun@mail.ru>
2024-03-06 18:25:59 +0700

    [back] feature: написал эндпоинт для создания комнаты + добавил проверку на наличие дубликатов комнаты

--- backend/models/room.py
+++ backend/models/room.py
@@ -4,6 +4,6 @@ from uuid import UUID, uuid4
 
 
 class Room(SQLModel, table=True):
-    room_id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
-    name: str = Field(index=True)
+    room_id: Optional[UUID] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True)
     blocked: bool = Field(default=False, index=True)
--- backend/routers/room.py
+++ backend/routers/room.py
@@ -1,5 +1,12 @@
+import uuid
+from typing import Annotated
 from uuid import UUID
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from starlette import status
+
+from backend.dependencies.database import get_db_session
 from backend.models.room import Room
 
 router = APIRouter(
@@ -9,26 +16,85 @@ router = APIRouter(
 )
 
 
+def uuid_generator():
+    id = uuid.uuid4()
+    return id
+
+async def select_room_by_name(session: Annotated[Session, Depends(get_db_session)], name: str):
+    statement = select(Room).where(Room.name==name)
+    rooms = session.exec(statement)
+    return rooms
+
+async def select_room_by_state( session: Annotated[Session, Depends(get_db_session)], blocked: bool):
+
+    if blocked == True:
+        statement = select(Room).where(Room.blocked==True)
+    else:
+        statement = select(Room).where(Room.blocked==False)
+    rooms = session.exec(statement)
+    return rooms
+
+async def select_all_rooms_default(session: Annotated[Session, Depends(get_db_session)]):
+
+    statement = select(Room)
+    rooms = session.exec(statement).all()
+    return rooms
+
+async def select_all_rooms_by_filters(name: str, blocked: bool,
+                                      session: Annotated[Session, Depends(get_db_session)]):
+
+    if blocked==True:
+        statement = select(Room).where(Room.name==name, Room.blocked==True )
+    else:
+        statement = select(Room).where(Room.name==name, Room.blocked==False )
+    rooms = session.exec(statement)
+    return rooms
+
 @router.get("/")
-async def get_all_rooms() -> list[Room]:
-    pass
+async def get_all_rooms(session: Annotated[Session, Depends(get_db_session)],
+                        name: str | None = None, blocked: bool | None = None):
+    params = []
+    if name is not None:
+        params.append(Room.name==name)
+    if blocked is not None:
+        params.append(Room.blocked==blocked)
+
+    statement = select(Room).where(*params)
+    rooms = session.exec(statement)
+    rooms = rooms.all()
+    return rooms
 
 
 @router.post("/")
-async def create_room() -> Room:
-    pass
+async def create_room(name: str,
+                      room_id: Annotated[UUID, Depends(uuid_generator)],
+                      session: Annotated[Session, Depends(get_db_session)]):
+    room = session.exec(select(Room).where(Room.name==name))
+    if room.all():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The room with name:{name} already exists in the system."
+        )
+    else:
+        room = Room(room_id=room_id, name=name) #TODO:проверка на наличие кабинета с занятым названием
+        session.add(room)
+        session.commit()
+    return room.room_id
 
 
+#TODO:если room_id не найден-> выкинуть ошибку
 @router.post("/{room_id}/give/{user_id}")
 async def give_room(room_id: UUID, user_id: UUID):
+    #если user_id не найден-> выкинуть ошибку
+
     pass
 
-
+#TODO:если room_id не найден-> выкинуть ошибку
 @router.put("/{room_id}")
 async def set_room_availability(room_id: UUID, availability: bool):
     pass
 
-
+#TODO:если room_id не найден-> выкинуть ошибку
 @router.delete("/{room_id}")
 async def delete_room(room_id: UUID):
     pass
