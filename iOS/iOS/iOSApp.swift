0149bc8bb9a253457ebda6343279e130151cd086
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-03-09 17:02:54 +0700

    [iOS][init] Init empty SwiftUI project

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by HITSStudent on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
