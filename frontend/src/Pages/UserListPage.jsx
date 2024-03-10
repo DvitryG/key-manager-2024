486c37a4ce1a173f64443898560e09c3c6ead5fc
yosef <yosef.eldesoky@gmail.com>
2024-03-10 18:26:19 +0700

    login profile and registeration are made dynamic

--- frontend/src/Pages/UserListPage.jsx
+++ frontend/src/Pages/UserListPage.jsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import UserSearch from '../Components/UserSearch';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import UserList from '../Components/UserList';
 
-
 function UserListPage (){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const accessToken = localStorage.getItem('accessToken');
+                const response = await axios.get('http://0.0.0.0:5000/users/?page=0&page_size=10', {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'accept': 'application/json'
+                    }
+                });
+                setUsers(response.data.users);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+                setError('Error fetching users. Please try again.');
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
     return (
         <div>
             <div className="request-header p-4 ps-5">
                 <h1>Список пользователей</h1>
             </div>
-         <UserSearch/> 
-          <div className="container d-flex justify-content-center flex-column w-75">
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="студент, преподаватель"/>
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="преподаватель"/>
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="студент, преподаватель"/>
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="деканат"/>
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="преподаватель"/>
-          <UserList fullName="Иванов Иван Иванович" email="name@example.com" status="админ"/>
-
-          </div>
-     
-     </div>
+            <div className="container d-flex justify-content-center flex-column w-75">
+                {loading ? (
+                    <p>Loading...</p>
+                ) : error ? (
+                    <p>{error}</p>
+                ) : (
+                    <div>
+                        {users.map(user => (
+                            <UserList
+                                key={user.user_id}
+                                fullName={user.name}
+                                email={user.email}
+                                status={user.roles_str}
+                            />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
+
 export default UserListPage;
